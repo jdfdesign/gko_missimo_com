1d7debcca9c1703e8e524f98746febe764eeb3f1
Regis Bruggheman <regis@joufdesign.com>
2015-08-28 10:47:16 +0200

    bump

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -2,5 +2,5 @@ WickedPdf.config = {
   #:wkhtmltopdf => '/Users/bruggheman/.rvm/gems/ruby-1.8.7-p374/bin/wkhtmltopdf',
   #:layout => "pdf.html",
   #:exe_path => '/Users/bruggheman/.rvm/gems/ruby-1.8.7-p374/bin/wkhtmltopdf'
-  :exe_path => '/home/missim/ruby/gems/bin'
+  :exe_path => '/home/missim/ruby/gems/bin/wkhtmltopdf'
 }
